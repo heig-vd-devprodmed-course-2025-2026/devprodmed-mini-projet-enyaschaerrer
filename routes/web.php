7ec6190a5a5a8d31602c206778b75de6a3c09ec3
enyaschaerrer <enya.schaerrer@gmail.com>
2026-02-24 11:52:28 +0100

    Créer le modèle User et la migration associée

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+// ... autres routes ...
+Route::get('/test-user', function () {
+    $user = new User();
+
+    $user->first_name = 'John';
+    $user->last_name = 'Doe';
+    $user->username = 'johndoe';
+    $user->email = 'johndoe@example.com';
+
+    $user->save();
+
+    return $user;
 });
