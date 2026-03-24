f2fbea175ea4fbea21d3509c0f1ad59b86e3492c
enyaschaerrer <enya.schaerrer@heig-vd.ch>
2026-03-24 15:22:37 +0100

    modifier bdd et modèles pour inscription et connexion

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class User extends Model
+class User extends Authenticatable
 {
     /**
  * Get the posts for the user.
