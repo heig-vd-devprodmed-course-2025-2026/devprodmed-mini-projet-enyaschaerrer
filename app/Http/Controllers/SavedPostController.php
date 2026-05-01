d76358c19f058f9ac3b2f060a66560487346256b
enyaschaerrer <enya.schaerrer@heig-vd.ch>
2026-05-01 22:12:02 +0200

    détail dans le controlleur de saved posts

--- app/Http/Controllers/SavedPostController.php
+++ app/Http/Controllers/SavedPostController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Post;
 use App\Models\SavedPost;
+use Illuminate\Support\Facades\Gate;
 
 class SavedPostController extends Controller
 {
